ddb55ce172923ef958c184899503aa315020d369
Harikrishnan Midhun <harikrishnanmidhun77@gmail.com>
2023-02-26 20:53:08 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useReducer, useContext } from "react";
+import { useInput } from "./useInput";
+// import { FruitsContext } from ".";
+import { useFruits } from ".";
 
 function App() {
+  const onSubmitFn = (e) => {
+    e.preventDefault();
+    alert(`you typed ${textInputProps.value}`);
+    textRevertFn();
+  };
+  const initialState = {
+    message: "hello",
+  };
+  const reducer = (state, action) => {
+    switch (action.type) {
+      case "yell":
+        return {
+          message: "heyyyyy",
+        };
+      case "whisper":
+        return {
+          message: "shhhh",
+        };
+      default:
+        return {
+          message: "hi",
+        };
+    }
+  };
+  const [state, dispach] = useReducer(reducer, initialState);
+  const [textInputProps, textRevertFn] = useInput("");
+  const [number, setNumber] = useReducer((number, newNumber) => {
+    return number + newNumber;
+  }, 0);
+
+  // const { fruits } = useContext(FruitsContext);
+  const { fruits } = useFruits();
+  console.log("fruits", fruits);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <form>
+        <input type="text" placeholder="color Name" {...textInputProps} />
+        <input type="color" />
+        <button onClick={onSubmitFn}>Submit </button>
+      </form>
+      <h1>{state.message}</h1>
+      <button
+        onClick={() => {
+          dispach({ type: "yell" });
+        }}
+      >
+        Yell{" "}
+      </button>
+      <button
+        onClick={() => {
+          dispach({ type: "whisper" });
+        }}
+      >
+        whisper{" "}
+      </button>
+
+      <br />
+      <h1 onClick={() => setNumber(1)}>{number}</h1>
+      <br />
+      <ul>
+        {fruits.map((f) => (
+          <li key={f.id}>{f.name}</li>
+        ))}
+      </ul>
+    </>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,14 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { createContext, useContext } from "react";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+//export const FruitsContext = createContext();
+const FruitsContext = createContext();
+export const useFruits = () => useContext(FruitsContext);
+
+const fruits = [
+  { id: 1, name: "Orange" },
+  { id: 2, name: "Mango" },
+  { id: 3, name: "Grapes" },
+  { id: 4, name: "Banana" },
+  { id: 5, name: "Peach" },
+];
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
+  <FruitsContext.Provider value={{ fruits }}>
     <App />
-  </React.StrictMode>
+  </FruitsContext.Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
